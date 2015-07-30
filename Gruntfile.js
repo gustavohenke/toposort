618abb136062746edb881453e6c538ba01ba3b38
novacrazy <novacrazy@gmail.com>
2015-07-30 04:13:05 -0500

    run eslint on Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,12 @@
 module.exports = function( grunt ) {
 
-    grunt.loadNpmTasks( 'grunt-babel' );
-    grunt.loadNpmTasks( 'grunt-contrib-clean' );
-    grunt.loadNpmTasks( 'grunt-banner' );
+    grunt.loadNpmTasks( "grunt-babel" );
+    grunt.loadNpmTasks( "grunt-contrib-clean" );
+    grunt.loadNpmTasks( "grunt-banner" );
 
-    var LICENSE = '/****\n * ' +
-                  grunt.file.read( './LICENSE', {encoding: 'utf-8'} ).replace( /\n/ig, '\n * ' ) +
-                  '\n ****/';
+    var LICENSE = "/****\n * " +
+                  grunt.file.read( "./LICENSE", {encoding: "utf-8"} ).replace( /\n/ig, "\n * " ) +
+                  "\n ****/";
 
     grunt.initConfig( {
         babel:     {
@@ -22,40 +22,40 @@ module.exports = function( grunt ) {
                 options: {
                     loose:    "all",
                     optional: [
-                        'spec.undefinedToVoid',
-                        'minification.constantFolding',
-                        'minification.propertyLiterals',
-                        'es7.classProperties'
+                        "spec.undefinedToVoid",
+                        "minification.constantFolding",
+                        "minification.propertyLiterals",
+                        "es7.classProperties"
                     ]
                 },
                 files:   [{
                     expand: true,
-                    cwd:    './src/',
-                    src:    './**/*.js',
-                    dest:   './build/'
+                    cwd:  "./src/",
+                    src:  "./**/*.js",
+                    dest: "./build/"
                 }]
             }
         },
         usebanner: {
             license: {
                 options: {
-                    position:  'top',
+                    position: "top",
                     banner:    LICENSE,
                     linebreak: true
                 },
                 files:   {
-                    src: ['./build/**/*.js']
+                    src: ["./build/**/*.js"]
                 }
             }
         },
         clean:     {
             build: {
-                src: ['./build']
+                src: ["./build"]
             }
         }
     } );
 
-    grunt.registerTask( 'build', ['clean:build', 'babel:build', 'usebanner:license'] );
+    grunt.registerTask( "build", ["clean:build", "babel:build", "usebanner:license"] );
 
-    grunt.registerTask( 'default', ['build'] );
+    grunt.registerTask( "default", ["build"] );
 };
